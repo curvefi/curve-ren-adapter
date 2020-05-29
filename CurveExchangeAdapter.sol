f5985c582bb49cec959be554252323f5c584de89
AA <angelangelov97@gmail.com>
2020-05-30 02:24:11 +0300

    add deposit/withdraw

--- CurveExchangeAdapter.sol
+++ CurveExchangeAdapter.sol
@@ -812,7 +812,21 @@ interface IUniswapExchange {
 
 interface ICurveExchange {
     function exchange(int128 i, int128 j, uint256 dx, uint256 min_dy) external;
+
     function get_dy(int128, int128 j, uint256 dx) external view returns (uint256);
+
+    function calc_token_amount(uint256[2] calldata amounts, bool deposit) external returns (uint256 amount);
+
+    function add_liquidity(uint256[2] calldata amounts, uint256 min_mint_amount) external;
+
+    function remove_liquidity(
+        uint256 _amount,
+        uint256[2] calldata min_amounts
+    ) external;
+
+    function remove_liquidity_imbalance(uint256[2] calldata amounts, uint256 max_burn_amount) external;
+
+    function remove_liquidity_one_coin(uint256 _token_amounts, int128 i, uint256 min_amount) external;
 }
 
 contract CurveExchangeAdapter is GSNRecipient {
@@ -885,6 +899,51 @@ contract CurveExchangeAdapter is GSNRecipient {
             // Send renBTC to the User instead
             RENBTC.transfer(_wbtcDestination, mintedAmount);
         }
+    }
+
+    function mintThenDeposit(address payable _wbtcDestination, uint256[2] amounts, uint256 min_mint_amount, uint256 slippage) external {
+        // Mint renBTC tokens
+        bytes32 pHash = keccak256(abi.encode(amounts, min_mint_amount, _wbtcDestination));
+        uint256 mintedAmount = registry.getGatewayBySymbol("BTC").mint(pHash, amounts[0], _nHash, _sig);
+
+        uint256 calc_token_amount = exchange.calc_token_amount(amounts, true);
+        uint256 min_mint_amount_now = calc_token_amount.mul(99).div(100)
+        if(min_mint_amount_now >= min_mint_amount) {
+            exchange.add_liquidity(amounts, min_mint_amount_now);
+        }
+        else {
+            RENBTC.transfer(_wbtcDestination, mintedAmount);
+        }
+    }
+
+    function removeLiquidityThenBurn(bytes calldata _btcDestination, uint256 amount, uint256[2] calldata min_amounts) external {
+        uint256 startRenbtcBalance = RENBTC.balanceOf(address(this));
+        exchange.remove_liquidity(amount, min_amounts);
+        uint256 endRenbtcBalance = RENBTC.balanceOf(address(this));
+        uint256 renbtcWithdrawn = endRenbtcBalance.sub(startRenbtcBalance);
+
+        // Burn and send proceeds to the User
+        registry.getGatewayBySymbol("BTC").burn(_btcDestination, renbtcWithdrawn);
+    }
+
+    function removeLiquidityImbalanceThenBurn(bytes calldata _btcDestination, uint256[2] calldata amounts, uint256 max_burn_amount) {
+        uint256 startRenbtcBalance = RENBTC.balanceOf(address(this));
+        exchange.remove_liquidity_imbalance(amounts, max_burn_amount);
+        uint256 endRenbtcBalance = RENBTC.balanceOf(address(this));
+        uint256 renbtcWithdrawn = endRenbtcBalance.sub(startRenbtcBalance);
+
+        // Burn and send proceeds to the User
+        registry.getGatewayBySymbol("BTC").burn(_btcDestination, renbtcWithdrawn);
+    }
+
+    function removeLiquidityOneCoin(bytes calldata _btcDestination, uint256 _token_amounts, int128 i, uint256 min_amount) {
+        uint256 startRenbtcBalance = RENBTC.balanceOf(address(this));
+        exchange.remove_liquidity_one_coin(_token_amounts, i, min_amount);
+        uint256 endRenbtcBalance = RENBTC.balanceOf(address(this));
+        uint256 renbtcWithdrawn = endRenbtcBalance.sub(startRenbtcBalance);
+
+        // Burn and send proceeds to the User
+        registry.getGatewayBySymbol("BTC").burn(_btcDestination, renbtcWithdrawn);
     }
     
     function swapThenBurn(bytes calldata _btcDestination, uint256 _amount, uint256 _minRenbtcAmount) external {
